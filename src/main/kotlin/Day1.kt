f466cce3069818f9ea1145790cfee4a8277befca
Roland Egiazaryan <roland.yeghiazaryan@gmail.com>
2022-12-02 00:27:19 +0100

    Add input from file method

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -61,3 +61,4 @@ private fun readStrings() = readLn().split(" ") // list of strings
 private fun readInts() = readStrings().map { it.toInt() } // list of ints
 private fun readLongs() = readStrings().map { it.toLong() } // list of longs
 private fun readDoubles() = readStrings().map { it.toDouble() } // list of doubles
+private fun readInput(name: String) = File("files", "$name.txt").readLines() // Reads lines from the given input txt file.
